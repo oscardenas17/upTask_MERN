694ec06b65b51e61cdfc2c75fa8ad0415e2b49f9
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-06-11 18:54:43 -0500

    Evitando emails duplicados

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -3,6 +3,16 @@ import Usuario from "../models/Usuarios.js";
 
 const registrar = async (req,res)=>{
     //console.log(req.body);
+    //Evitar registro mail duplicado
+    const {email} = req.body
+    const existeUsuario = await Usuario.findOne( {email} )
+    console.log('existe usuario');
+
+    if(existeUsuario){
+        const error = new Error('Email ya registrado');
+        return res.status(400).json( {msg:error.message} )
+    }
+
     try {
         //console.log(usuario);
         const usuario = new Usuario(req.body)
