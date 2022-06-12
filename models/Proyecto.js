4e7ad2cbce1cf4ff7155d74b70a3a633043e913e
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-06-12 17:43:57 -0500

    Creando el Modelo de proyecto

---
+++ models/Proyecto.js
@@ -0,0 +1,27 @@
+import moongose from "mongoose";
+
+const proyectoSchema = mongoose.Schema( {
+    nombre: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    descripcion: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    fechaEntrega: {
+        type: Date,
+        default: Date.now(),
+    },
+    cliente: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    creador: {
+        type: moongose.Schema.Types.ObjectId
+    }
+
+});
