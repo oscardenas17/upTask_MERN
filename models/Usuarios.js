743230b2b5e43f75879795cdf9348792d0dd8a0a
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-06-11 20:17:37 -0500

    Hashear los password

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import  bcrypt  from "bcrypt"; 
 
 const usuarioSchema = mongoose.Schema(
   {
@@ -32,6 +33,19 @@ const usuarioSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+//Con pre se ejecuta el sig code antes de guardar en la bd
+usuarioSchema.pre('save', async function(next) {
+
+  //Si no esta modificando password- next - al siguiente middleware
+  if(!this.isModified('password')){
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+
+  this.password = await bcrypt.hash(this.password, salt)
+} )
 
 //definir modelo
 const Usuario = mongoose.model("Usuario", usuarioSchema);
