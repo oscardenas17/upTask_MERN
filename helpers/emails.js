b1ed141d8e5acadf43bd375cc186faf9e1506f5e
oscardenas17 <oscardenas17@gmail.com>
2022-06-25 10:06:55 -0500

    Enviando mail de bienvenida

--- helpers/emails.js
+++ helpers/emails.js
@@ -1,6 +1,34 @@
 import nodemailer from 'nodemailer'
 
 //datos = mail, token, y nombre usuario
-export const emailRegistro = (datos) =>{
-console.log('Datos', datos);
+export const emailRegistro =  async(datos) =>{
+    //console.log('Datos', datos);
+    const {email, nombre, token} = datos
+
+    const transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "b8f82a8e89f9af",
+          pass: "a6e9592afca0c3"
+        }
+      });
+
+      //Información del email
+      const info = await transport.sendMail({
+        from: ' "Administrador Uptask" <cuentas@uptask.com> ',
+        to: email,
+        subject: "Comprueba tu cuenta",
+        text: "Comprueba tu cuenta",
+        html: `
+            <p style="font-size: 12px;">Hola: ${nombre }, ya puedes comprobar tu cuenta</p>
+            <p>Tu cuenta ya esta casi lista, compruebala en el siguiente enlace: </p>
+
+
+            <a  href=" ${process.env.FRONTEND_URL}/confirmar/${token} "> Comprobar Cuenta </a>
+
+
+            <p>Si tu no creaste esta cuenta, puedes ignorar el mensaje</p>
+        `
+      })
 }
