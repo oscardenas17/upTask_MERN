ca2a79aa05fec9a600fdc31cfbc098099ba1433c
oscardenas17 <oscardenas17@gmail.com>
2022-07-18 22:01:05 -0500

    Actualizar una tarea

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -34,37 +34,65 @@ const agregarTarea = async (req,res) =>{
 //obtener una tarea de la persona autenticada y que haya creado el proyecto
 const obtenerTarea = async (req,res) =>{
     
-    //Obtener el id de la tarea
+    //Obtener el id de la tarea, identificarla
     const {id} = req.params;
     //console.log(id);
-    //Identidicar la tarea, con populate cruzamos la info de proyecto para encontrar el creador del proyecto
+    //Identidicar la tarea, con populate cruzamos la info de proyecto para encontrar el creador del proyecto - 
     const tarea = await Tarea.findById(id).populate("proyecto");
     //console.log(tarea); 
-
+    //consultar si existe
     if(!tarea){
         const error = new Error("tarea no encontrada");
         return res.status(404).json({msg:error.message});
     }
 
-
+    //verificar qu eel usuario tenga permisos
     if(tarea.proyecto.creador.toString() !== req.usuario._id.toString()){
         const error = new Error("Acción no válida");
         return res.status(403).json({msg:error.message});
     }
 
-
+    res.json(tarea)
 }
 
 
 
 const actualizarTarea = async (req,res) =>{
-    
+      //Obtener el id de la tarea, identificarla
+      const {id} = req.params;
+      //console.log(id);
+      //Identidicar la tarea, con populate cruzamos la info de proyecto para encontrar el creador del proyecto - 
+      const tarea = await Tarea.findById(id).populate("proyecto");
+      //console.log(tarea); 
+      //consultar si existe
+      if(!tarea){
+          const error = new Error("tarea no encontrada");
+          return res.status(404).json({msg:error.message});
+      }  
+      //verificar qu eel usuario tenga permisos
+      if(tarea.proyecto.creador.toString() !== req.usuario._id.toString()){
+          const error = new Error("Acción no válida");
+          return res.status(403).json({msg:error.message});
+      }
+      tarea.nombre = req.body.nombre || tarea.nombre;
+      tarea.descripcion = req.body.descripcion || tarea.descripcion;
+      tarea.prioridad = req.body.prioridad || tarea.prioridad;
+      tarea.fechaEntrega = req.body.fechaEntrega || tarea.fechaEntrega;
+
+      try {
+        const tareaAlmacenada = await tarea.save();
+        res.json(tareaAlmacenada);
+      } catch (error) {
+        console.log(error);
+      }
 };
+ 
 
 
 const eliminarTarea = async (req,res) =>{
     
 };
+
 
 
 const cambiarEstado = async (req,res) =>{
